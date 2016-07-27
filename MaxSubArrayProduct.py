062725950bbdaa39582793ca7d4589907245e273
Lynn <liu.silin@foxmail.com>
2016-07-27 09:08:58 +0800

    Add a file

---
+++ MaxSubArrayProduct.py
@@ -0,0 +1,31 @@
+# Uses python2
+"""
+  Program for Coursera   Algorithm Toolbox
+
+  Author: Lynn Lau
+  Date: 2016/07/25
+"""
+
+N = int(raw_input(''))
+n = raw_input('')
+List = [int (x)  for x in n.split()]
+#print List
+
+FirstMaximumNum = max(List)
+List.remove(FirstMaximumNum)
+SecondMaximumNum = max(List)
+
+#print FirstMaximumNum
+#print SecondMaximumNum
+result = SecondMaximumNum * FirstMaximumNum
+print result
+
+""" Using for loop makes excution time over the
+    limit time."""
+#result = 0
+#for i in range(0, N):
+#	for j in range(i+1, N):		
+#		if List[i]*List[j] > result:
+#		    result = List[i]*List[j]
+#print result
+ 
