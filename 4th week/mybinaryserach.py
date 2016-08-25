dad22a2a70bae06f1c36cb0836750601e182caf8
Lynn <liu.silin@foxmail.com>
2016-08-25 19:43:00 +0800

    update the code

--- 4th week/mybinaryserach.py
+++ 4th week/mybinaryserach.py
@@ -1,5 +1,5 @@
 # Uses python2
-
+# -*- coding: utf-8 -*-
 # binary search
 '''
 A simple program for Coursera Data structures and Algorithms
@@ -10,29 +10,34 @@ Date:2016/08/24
 
 import sys
 
-def my_binary_search(value, low, high, keys):
 
-    midValue = int((low + high)/2) + low
-    for key in keys:
-        if key == value[midValue]:
-            return midValue
+def my_binary_search(value, key):
+
+    low = 0
+    high = len(value) - 1
+    while(low <= high):
+        mid = (low + high)/2
+        midval = value[mid]
+
+        if midval < key:
+            low = mid + 1
+        elif midval > key:
+            high = mid - 1
         else:
-            if key < value[midValue]:
-                high = midValue - 1
-                # return my_binary_search(value, low, midValue-1, key)
-            else:
-                low = midValue + 1
-                # return my_binary_search(value, midValue+1, high, key)
-        # key = key + 1
+            return mid
+
+    return -1
 
 if __name__ =='__main__':
-    valueInput = list(map(int, raw_input().split()))
-    number = valueInput[0]
-    value = valueInput[1: :1]
-    keyInput = list(map(int, raw_input().split()))
-    keys = keyInput[1: :1]
-    low = 0
-    high = number-1
 
-    print my_binary_search(value, low, high, keys)
-    # print number, value, print keys, print low, print high
+    data1 = list(map(int, raw_input().split()))
+    # values list
+    data2 = list(map(int, raw_input().split()))
+    # keys list
+    n = data1[0]
+    m = data2[0]
+    # result = list()
+    a = data1[1 : m + 1]
+    for x in data2[1: m + 1]:
+        # replace with the call to binary_search when implemented
+        print my_binary_search(a, x)
